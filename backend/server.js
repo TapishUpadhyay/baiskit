8ec8e4cf41fd11774987679a1c76f4a5b7083c77
Aryan <manasawasthi960@gmail.com>
2026-08-24 19:29:09 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.get("/api", (req, res) => {
   res.json({
     message: "Baiskit API is working!"
